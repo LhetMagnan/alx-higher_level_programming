d38d5058908c136399d64425d897c721c423b644
LhetMagnan <lmagnan17@alustudent.com>
2021-09-07 22:03:37 +0000

    0x01-python-if_else_loops_functions file added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
-        print("{:02}".format(number), end=", ")
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
