acebb777211cf0e9ab6eb9c01e065e7bc06b17b8
LhetMagnan <lmagnan17@alustudent.com>
2021-09-07 21:59:11 +0000

    0x01-python-if_else_loops_functions file added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number < 10:
-        print("0{}, ".format(number), end="")
+        print("{:02}".format(number), end=", ")
     else:
-        print("{}, ".format(number), end="")
+        print("{}".format(number), end=", ")
