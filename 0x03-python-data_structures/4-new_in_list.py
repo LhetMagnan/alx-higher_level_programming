ed96d377d569ede6582af064257263835419a795
LhetMagnan <lmagnan17@alustudent.com>
2021-09-13 23:57:57 +0000

    0x03-python-data_structures files update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    replaced = []
-    for i in range(len(my_list)):
-        replaced.append(my_list[i])
-    if idx < 0:
-        return replaced
-    elif idx >= len(my_list):
-        return replaced
-    else:
-        replaced [idx] = element
-        return replaced
+    list1 = list(my_list)
+    if idx < 0 or idx > len(my_list)-1:
+        return list1
+    list1[idx] = element
+    return list1
