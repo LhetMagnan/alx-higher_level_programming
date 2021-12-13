e05309581e119a34aa819ec2af946c8f4c5d13b9
LhetMagnan <lmagnan17@alustudent.com>
2021-12-13 19:33:20 +0000

    0x12-javascript-warm_up files added

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (process.argv.slice(2).length <= 0) {
-    console.log("No argument");
+    console.log('No argument');
 }
 else if (process.argv.slice(2).length === 1) {
-    console.log("Argument found");
+    console.log('Argument found');
 }
 else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
